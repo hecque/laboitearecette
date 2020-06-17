ac80ca4c7fe07469c5dc71acd5374339b86b38e6
hecque <50669769+hecque@users.noreply.github.com>
2020-06-17 06:24:08 +0200

    Adding header

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const header = ({ pseudo }) => {
+
+    const formatPseudo = pseudo => /[aeiouy]/i.test(pseudo[0]) ? `d'${ pseudo }` : `de ${ pseudo }`;
+
+    return (
+        <header>
+            <h1>La boîte à recettes { formatPseudo(pseudo) }</h1>
+        </header>
+    )
+}
+
+export default header;
